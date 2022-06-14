0f8372e0cb7e06e8ee990e6103e8f96889576403
Dongsoo Kim <william@uzen.io>
2022-06-14 09:31:21 +0900

    env setting
    env setting

---
+++ src/main/java/com/daniel/blacksmith/BlacksmithApp.java
@@ -0,0 +1,11 @@
+package com.daniel.blacksmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlacksmithApp {
+    public static void main(String ... args){
+        SpringApplication.run(BlacksmithApp.class, args);
+    }
+}
