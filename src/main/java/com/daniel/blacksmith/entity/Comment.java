1a129f90a9f8bc275c4ba0f6e66f7f7c6c4fba9c
Dongsoo Kim <william@uzen.io>
2022-06-14 09:51:24 +0900

    add Entity(Post, Comment)
    add Entity(Post, Comment)

---
+++ src/main/java/com/daniel/blacksmith/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.daniel.blacksmith.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(name="comment_name")
+    private String commentName;
+    @Column(name="comment_email")
+    private String commentEmail;
+    @Column(name="comment_body")
+    private String commentBody;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable = false)
+    private Post post;
+
+}
