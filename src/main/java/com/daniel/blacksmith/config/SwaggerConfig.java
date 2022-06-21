03472e94520c96559048bf04fd67d16c762258bd
Dongsoo Kim <william@uzen.io>
2022-06-21 23:19:02 +0900

    swagger setting
    swagger setting

--- src/main/java/com/daniel/blacksmith/config/SwaggerConfig.java
+++ src/main/java/com/daniel/blacksmith/config/SwaggerConfig.java
@@ -5,15 +5,24 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey(){
+        //1. name, 2. keyname, 3. pass as header
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
 
     private ApiInfo apiInfo(){
         return new ApiInfo(
@@ -32,9 +41,22 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
             .apiInfo(apiInfo())
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
             .select()
             .apis(RequestHandlerSelectors.any())// look for any apis OR .apis(RequestHandlerSelectors.basePackage("postController"))
             .paths(PathSelectors.any()) //.paths(PathSelectors.regex("url"))
             .build();
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
